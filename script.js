0a419e786f3c1bb5fd7b9f04a8be783178d961ba
JezyYoo <volchevdanya@gmail.com>
2022-05-12 19:10:25 +0300

    coppied to buffer by click

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var encryptedValue = "";
-var decryptedValue="";
+var decryptedValue= "";
 
 $("#submit").on("click", ()=>{
     encryptedValue = $("#encrypted-packge").val();
-    decryptedValue = encryptedValue.replace(/[\])}[{(]/g, '');
-    $("#decrypted-pacakge").val(decryptedValue);
+    decryptedValue = encryptedValue.replace(/([\])}[{(])/g, '');
+    $("#encrypted-packge").val(decryptedValue);
+    window.navigator.clipboard.writeText(decryptedValue)
 })
+
+function copytext(el) {
+    var $tmp = $("<input>");
+    $("body").append($tmp);
+    $tmp.val($(el).text()).select();
+    console.log($(el).text())
+    // document.execCommand("copy");
+    $tmp.remove();
+}
